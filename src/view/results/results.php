a74e2a51158b942c0ecf49d52e5ff09a6f91eedb
JulianKilner <jpa.kilner@gmail.com>
2019-04-16 14:17:28 +0200

    ajout de la page des resultats

---
+++ src/view/results/results.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Results</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1 class="text-center">Resultats du tournoi :</h1>
+        <div class="container">
+            <div class="row">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name :</th>
+                            <th scope="col">Classement : </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php for ($tableIndex=0; $tableIndex < 8; $tableIndex++){ ?>
+                        <tr>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </th>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
