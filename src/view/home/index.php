4cf35b2c6051e0ff869e54e7013170967f91780c
CTRL <inoxsdev@gmail.com>
2019-04-16 11:10:23 +0200

    main page

---
+++ src/view/home/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Page Article</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<h1 class="text-center">Tournoi de pétanque</h1>
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <button type="button" class="btn btn-success">Créer un tournoi</button>
+        </div>
+    </div>
+</div>
+</body>
+</html>
