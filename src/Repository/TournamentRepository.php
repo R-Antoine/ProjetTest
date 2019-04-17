8313cc66c643cb20b3b978329d9ed5f4c71d94b9
R-Antoine <all4one-flood@live.fr>
2019-04-17 14:46:44 +0200

    ok sa peu marché

---
+++ src/Repository/TournamentRepository.php
@@ -0,0 +1,67 @@
+<?php
+/* Created by PhpStorm.
+ * User: HB
+ * Date: 17/04/2019
+ * Time: 14:16
+ */
+
+namespace App\Repository;
+
+
+use App\Model\Tournament;
+
+class TournamentRepository extends Repository implements IRepository
+{
+    private static $table = 'tournament';
+
+    /* TournamentRepository constructor.
+     */
+    public function construct()
+    {
+        parent::construct(TournamentRepository::$table);
+    }
+
+
+    public function getResult(string $request = ''): ?Tournament
+    {
+        $tournament = null;
+        $result = parent::getResult($request);
+
+        if ($result) {
+            $tournament = new Tournament();
+            $tournament->setId($result['id']);
+        }
+        return $tournament;
+    }
+
+
+    public function getResults(string $request = ''): array
+    {
+        $tournaments = [];
+        $results = parent::getResults($request);
+
+        if ($results) {
+            foreach ($results as $result) {
+                $tournament = new Tournament();
+                $tournament->setId($result['id']);
+                $tournaments[] = $tournament;
+            }
+        }
+
+        return $tournaments;
+    }
+
+    /**
+     * @param Tournament $tournament
+     * @return mixed
+     * @throws \Exception
+     */
+    public function insert($tournament)
+    {
+        if (!$tournament instanceof Tournament) {
+            throw new \Exception('You can save only tournaments');
+        }
+        $request = "(id) VALUES ('" . $tournament->getId()."')";
+        return parent::insert($request);
+    }
+}
