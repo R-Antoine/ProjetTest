48c9f4a3fd3a6369df5633ba21425b7a7cae8ba9
JulianKilner <jpa.kilner@gmail.com>
2019-04-17 10:27:11 +0200

    fix de la création de la bdd

--- db.php
+++ db.php
@@ -19,36 +19,41 @@ $db = new mysqli(
 );
 
 $db->query(
-    'CREATE TABLE IF NOT EXISTS team(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,name VARCHAR(32) NOT NULL);'
+    'CREATE TABLE IF NOT EXISTS team('.
+    'id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,'.
+    'name VARCHAR(32) NOT NULL'.
+    ');'
 );
 $db->query(
-    'CREATE TABLE IF NOT EXISTS player(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,name VARCHAR(32) NOT NULL,teamId INT NOT NULL,FOREIGN KEY (teamId) REFERENCES team(id));'
+    'CREATE TABLE IF NOT EXISTS player('.
+    'id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,'.
+    'name VARCHAR(32) NOT NULL,'.
+    'team_id INT NOT NULL,'.
+    'FOREIGN KEY (team_id) REFERENCES team(id)'.
+    ');'
 );
 $db->query(
-    'CREATE TABLE IF NOT EXISTS tournament(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,name VARCHAR(32) NOT NULL);'
+    'CREATE TABLE IF NOT EXISTS tournament('.
+    'id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,'.
+    'name VARCHAR(32) NOT NULL'.
+    ');'
 );
 $db->query(
-    'CREATE TABLE IF NOT EXISTS participation(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,teamId INT NOT NULL,tournamentId INT NOT NULL,FOREIGN KEY (teamId) REFERENCES team(id),FOREIGN KEY (tournamentId) REFERENCES tournament(id));'
+    'CREATE TABLE IF NOT EXISTS participation('.
+    'id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,'.
+    'team_id INT NOT NULL,'.
+    'tournament_id INT NOT NULL,'.
+    'FOREIGN KEY (team_id) REFERENCES team(id),'.
+    'FOREIGN KEY (tournament_id) REFERENCES tournament(id)'.
+    ');'
 );
-
-//if (!$db->query("INSERT INTO player(firstname,lastname) VALUES('Jean','Robert')")
-
-$db->query('INSERT INTO player(name, teamId) VALUES ("Jean", 0);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Robert", 0);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Rouquin", 1);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Guillaume", 1);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Antoine", 2);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Julian", 2);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Paul", 3);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Carine", 3);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Rui", 4);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Anïs", 4);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Pierre-alain", 5);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Amandine", 5);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Alexandra", 6);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Hinerava", 6);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Jhonny", 7);');
-$db->query('INSERT INTO player(name, teamId) VALUES ("Koba LaD", 7);');
+$db->query(
+    'INSERT INTO player(name, team_id) VALUES '.
+    '("Jean", 0), ("Robert", 0), ("Rouquin", 1), ("Guillaume", 1),'.
+    '("Antoine", 2), ("Julian", 2), ("Paul", 3), ("Carine", 3),'.
+    '("Rui", 4), ("Anïs", 4), ("Pierre-alain", 5), ("Amandine", 5),'.
+    '("Alexandra", 6), ("Hinerava", 6), ("Jhonny", 7), ("Koba LaD", 7);'
+);
 
 $db->close();
 
