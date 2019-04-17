fe12d3e5babbccc98ee25e17fe02ece9fab1f9d6
R-Antoine <all4one-flood@live.fr>
2019-04-17 16:50:02 +0200

    vous etes nul sur agario

--- src/view/tournament/index.php
+++ src/view/tournament/index.php
@@ -6,333 +6,49 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
+<h1 style="text-align:center;">TOURNOIS</h1>
 
 <body>
-<div class="header"><h1>Tournois de la tricke</h1></div>
-<div class="bracket-container">
-  <div class="bracket-level">
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Fordam</div><div class="bracket-score">44</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Sacred Heart</div><div class="bracket-score">22</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-name">Eastern Kentucky</div><div class="bracket-score">16</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-name">Indiana State</div><div class="bracket-score">36</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">UNI</div><div class="bracket-score">44</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Stephen F. Austin</div><div class="bracket-score">10</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Montana</div><div class="bracket-score">52</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">San Diego</div><div class="bracket-score">14</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">New Hampshire</div><div class="bracket-score">44</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Fordham</div><div class="bracket-score">19</div>
-      </div></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Chattanooga</div><div class="bracket-score">35</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Indiana State</div><div class="bracket-score">14</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Illinois State</div><div class="bracket-score">41</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">UNI</div><div class="bracket-score">21</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Eastern Washington</div><div class="bracket-score">37</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Montana</div><div class="bracket-score">20</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">New Hampshire</div><div class="bracket-score">35</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Chattanooga</div><div class="bracket-score">30</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-name">Illinois State</div><div class="bracket-score">59</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-name">Eastern Washington</div><div class="bracket-score">46</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team">
-        <div class="bracket-name">New Hampshire</div><div class="bracket-score">--</div>
-      </div>
-      <div class="bracket-team">
-        <div class="bracket-name">Illinois State</div><div class="bracket-score">--</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team">
-        <div class="bracket-name"></div>
-        <div class="bracket-score"></div>
-      </div>
-      <div class="bracket-team">
-        <div class="bracket-name">North Dakota State</div>
-        <div class="bracket-score">--</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-score">3</div><div class="bracket-name">Sam Houston State</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-score">35</div><div class="bracket-name">North Dakota State</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">34</div><div class="bracket-name">Sam Houston State</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score">31</div><div class="bracket-name">Villanova</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-score">32</div><div class="bracket-name">Coastal Carolina</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-score">39</div><div class="bracket-name">North Dakota State</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-score">26</div><div class="bracket-name">Jacksonville State</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-score">37</div><div class="bracket-name">Sam Houston State</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">29</div><div class="bracket-name">Villanova</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score">22</div><div class="bracket-name">Liberty</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">36</div><div class="bracket-name">Coastal Carolina</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score"15></div><div class="bracket-name">Richmond</div>
-      </div>
-    </div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">27</div><div class="bracket-name">North Dakota State</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score">24</div><div class="bracket-name">South Dakota State</div>
-      </div>
-    </div>
-  </div>
-  <div class="bracket-level">
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">21</div><div class="bracket-name">Sam Houston State</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score">17</div><div class="bracket-name">Southeastern Louisiana</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-score">21</div><div class="bracket-name">James Madison</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-score">26</div><div class="bracket-name">Liberty</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team winner">
-        <div class="bracket-score">46</div><div class="bracket-name">Richmond</div>
-      </div>
-      <div class="bracket-team loser">
-        <div class="bracket-score">24</div><div class="bracket-name">Morgan State</div>
-      </div>
-    </div>
-    <div class="bracket-matchup"></div>
-    <div class="bracket-matchup">
-      <div class="bracket-team loser">
-        <div class="bracket-score">40</div><div class="bracket-name">Montana State</div>
-      </div>
-      <div class="bracket-team winner">
-        <div class="bracket-score">47</div><div class="bracket-name">South Dakota State</div>
-      </div>
-    </div>
-  </div>
+<div class="container">
+<div class="row" style="margin-top:50px;">
+<div class="col-md-3">
+<span>TEAM 1</span><input type="number">
+<span>TEAM 2</span><input type="number">
 </div>
-
-<style>
-@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700);
-@import url(https://fonts.googleapis.com/css?family=Dosis);
-
-html, body{
-  with: 100%;
-  height: 100%;
-  margin: 0;
-  background-color: #E8E8E8;
-}
-
-.header{
-  height: 10%;
-}
-
-.header>h1{
-  margin: auto;
-  text-align: center;
-  font-family: 'Dosis', sans-serif;
-  color: #205060
-}
-
-.bracket-container{
-  display: flex;
-  flex-direction: row;
-  height: 90%;
-  width: 100%;
-}
-
-.bracket-level{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 100%;
-  width: 100%;
-  flex-grow: 1;
-  transition: all ease .5s;
-}
-
-.bracket-level:hover{
-  width: 200%;
-}
-
-.bracket-matchup{
-  width: 90%;
-  margin: auto;
-  max-height: 50px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.bracket-team{
-  height: 45%;
-  width: 100%;
-  background-color: #F5F5F5;
-  box-shadow: rgba(0, 0, 0, 0.3) 0 1px 3px;
-  display: flex;
-  flex-direction: row;
-  transition: all ease .5s;
-}
-
-.bracket-name{
-  font-family: 'Open Sans', sans-serif;
-  width: 75%;
-  font-size: .75em;
-  padding: .2em;
-  line-height: 1.5em;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  color: #2B2B2B;
-  text-align: center;
-}
-
-.bracket-score{
-  font-family: 'Open Sans', sans-serif;
-  font-size: .75em;
-  padding: .2em;
-  line-height: 1.5em;
-  white-space: nowrap;
-  overflow: hidden;
-  color: #2B2B2B;
-  text-align: center;
-}
-
-.winner>.bracket-name, .winner>.bracket-score{
-  font-weight: bold;
-  color: #D07030;
-}
-
-.bracket-team:hover{
-  background-color: #E8E8E8;
-  height: 100%;
-  transition: all ease .5s;
-}
-
-.bracket-team:hover>.bracket-name{
-  overflow: none;
-  text-overflow: clip;
-  white-space: normal;
-  line-height: 1em;
-  align-self: center;
-  width: 100%;
-}
-
-.bracket-team:hover>.bracket-score{
-  display: none;
-}
-</style>
+<div class="col-md-3">
+<span>TEAM 3</span><input type="number">
+<span>TEAM 4</span><input type="number">
+</div>
+<div class="col-md-3">
+<span>TEAM 5</span><input type="number">
+<span>TEAM 6</span><input type="number">
+</div>
+<div class="col-md-3">
+<span>TEAM 7</span><input type="number">
+<span>TEAM 8</span><input type="number">
+</div>
+</div>
+<div class="row" style="margin-top:50px; margin-left: 265px;">
+<div class="col-md-6">
+<span>TEAM 1</span><input type="number"><br>
+<span>TEAM 2</span><input type="number">
+</div>
+<div class="col-md-6">
+<span>TEAM 1</span><input type="number"><br>
+<span>TEAM 2</span><input type="number">
+</div>
+<div class="row" style="margin-left: 240px; margin-top: 66px;">
+<div class="col-md-12">
+<span>TEAM</span><input type="number"><br>
+<span>TEAM</span><input type="number">
+</div>
+</div>
+<div class="row" style="margin-right: 149px;">
+<div class="col-md-12">
+<span>TEAM</span><input type="number"><br>
+<span>TEAM</span><input type="number">
+</div>
+</div>
+</div>
 </body>
 </html>
