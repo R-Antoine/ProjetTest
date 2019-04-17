226b3a899e1100cf13bd3e2958567fa488a74881
R-Antoine <all4one-flood@live.fr>
2019-04-17 14:12:04 +0200

    julian arrete t'es commit claqué du cul stp MAMAN !

---
+++ src/view/bracket/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Page Article</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<h1 class="text-center">Tournois</h1>
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+        </div>
+    </div>
+</div>
+</body>
+</html>
